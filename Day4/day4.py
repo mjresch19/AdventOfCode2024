997564fce41165dde19ecbe588d9a65fe22cf405
reschmax <reschmax@msu.edu>
2025-01-14 21:43:44 -0500

    Created start to day 4 problem

---
+++ Day4/day4.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+def read_txt(filename: str):
+
+    file = open(filename, "r")
+
+    file_contents = file.readlines()
+
+    return file_contents
+
+
+if __name__ == "__main__":
+
+    #TOOD - finish this problem by checking if the report is safe
+    contents = read_txt("Day4/input.txt")
+
+    content_list = []
+    for i in range(len(contents)):
+        contents[i] = contents[i].strip("\n")
+        content_list.append(list(contents[i]))
+
+    pprint(content_list)
